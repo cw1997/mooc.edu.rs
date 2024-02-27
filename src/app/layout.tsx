d1fad87888432053f34b10469f9816597f4e7b79
Chang Wei <changwei1006@gmail.com>
2024-02-27 21:00:35 +0800

    build homepage by next.js
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "www.MOOC.edu.rs",
+  description: "A massive open online course (MOOC /muːk/) or an open online course is an online course aimed at unlimited participation and open access via the Web.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.sass";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>
+        <strong>M(assive) O(pen) O(nline) C(ourses)</strong> .edu.rs
+      </h1>
+      <div className={styles.description}>
+        <p>
+          A massive open online course (MOOC /muːk/) or an open online course is an online course aimed at unlimited participation and open access via the Web.
+        </p>
+      </div>
+      <div className={styles.contact}>
+        <p>
+          Email: <a href="mailto:changwei1006@gmail.com">changwei1006@gmail.com</a> (Chang Wei 昌維)
+        </p>
+      </div>
+    </div>
+  );
+}
